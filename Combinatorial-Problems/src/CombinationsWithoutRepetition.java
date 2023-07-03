4b4b0726c99be7aa03fbee0ac46a5d0376edc58f
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-07-03 17:40:28 +0300

    Combinations Without Repetition - solution

---
+++ Combinatorial-Problems/src/CombinationsWithoutRepetition.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class CombinationsWithoutRepetition {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String[] elements = sc.nextLine().split(" ");
+
+		int k = Integer.parseInt(sc.nextLine());
+
+		String[] slots = new String[k];
+
+		combinations(elements, slots, 0, 0);
+
+		sc.close();
+
+	}
+
+	private static void combinations(String[] elements, String[] slots, int index, int start) {
+		if (index == slots.length) {
+			print(slots);
+
+		} else {
+			for (int i = start; i < elements.length; i++) {
+				slots[index] = elements[i];
+
+				combinations(elements, slots, index + 1, i + 1);
+			}
+
+		}
+
+	}
+
+	private static void print(String[] permutes) {
+		System.out.println(String.join(" ", permutes));
+
+	}
+}
