84e89394a1572f411fce688bb998e5d54e413d1f
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-08-30 17:47:14 +0300

    Topological sorting - solution

--- Graphs/src/Main.java
+++ Graphs/src/Main.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Deque;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -70,6 +71,41 @@ public class Main {
 	}
 
 	public static Collection<String> topSort(Map<String, List<String>> graph) {
-		throw new AssertionError("Not Implemented");
+		Map<String, Integer> dependenciesCount = getDependenciesCount(graph);
+		List<String> sorted = new ArrayList<>();
+
+		while (!graph.isEmpty()) {
+			String key = graph.keySet().stream().filter(k -> dependenciesCount.get(k) == 0).findFirst().orElse(null);
+
+			if (key == null) break;
+
+			for (String child : graph.get(key)) {
+				dependenciesCount.put(child, dependenciesCount.get(child) - 1);
+			}
+
+			sorted.add(key);
+			graph.remove(key);
+		}
+		
+		if(!graph.isEmpty()) throw new IllegalArgumentException();
+		
+		
+		return sorted;
+
+	}
+
+	private static Map<String, Integer> getDependenciesCount(Map<String, List<String>> graph) {
+		Map<String, Integer> dependenciesCount = new LinkedHashMap<>();
+
+		for (Map.Entry<String, List<String>> node : graph.entrySet()) {
+			dependenciesCount.putIfAbsent(node.getKey(), 0);
+
+			for (String child : node.getValue()) {
+				dependenciesCount.put(child, dependenciesCount.getOrDefault(child, 0) + 1);
+			}
+
+		}
+
+		return dependenciesCount;
 	}
 }
